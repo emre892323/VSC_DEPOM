4abb486141682a87033451b4d1056716d07e05da
emre <147210921+emre892323@users.noreply.github.com>
2025-11-10 17:45:32 +0300

    ilk commit

---
+++ javaScript_dersleri/app.js
@@ -0,0 +1,28 @@
+// ! EKRANA YAZDIRMA KONUTLARI
+
+//? Write ve writeln
+
+// document.write("emre <br>");
+// document.write("emre");
+
+
+//? Console log ile consola yazdırma 
+
+// console.log("hello world");
+
+
+
+
+//? Alert ile kullanıcıya mesaj verme
+
+// alert("emre")
+
+var degisken=5 , a=100
+
+console.log("iki sayinin toplami="+(degisken+a))
+document.writeln(degisken+a);
+
+
+// var ile tanımladığımız  herşey function scope oluyor
+
+
